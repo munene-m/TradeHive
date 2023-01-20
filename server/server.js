5fb0cfa8895f27e7f080f2bb0dd110aa5763dae4
munene <munenenjue18@gmail.com>
2023-01-20 20:38:44 +0300

    added routes to be used by the express app

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const cors = require("cors")
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
 const port  = 3000
+const authRoute = require('./routes/authRoute')
+const serviceRoute = require('./routes/serviceRoute')
+const categoryRoute = require('./routes/categoryRoute')
 
 dotenv.config()
 app.use(express.json())
@@ -17,6 +20,10 @@ mongoose.connect(process.env.MONGO_URL, {
   .then(console.log("connected to MONGO"))
   .catch((err) => console.log(err));
 
+
+app.use("/auth", authRoute)
+app.use("/services", serviceRoute)
+app.use("/categories", categoryRoute)
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
