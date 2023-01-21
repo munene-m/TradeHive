004b27a3c8658db289ce7faaa5ed97578b7e925f
munene <munenenjue18@gmail.com>
2023-01-21 13:19:11 +0300

    made styling changes

--- server/controllers/serviceController.js
+++ server/controllers/serviceController.js
@@ -1,4 +1,5 @@
 const serviceModel = require('../models/Service')
+const authModel = require('../models/Auth')
 const asyncHandler = require('express-async-handler')
 
 //create new service
@@ -9,13 +10,7 @@ const createService = asyncHandler(async (req, res) => {
         throw new Error("Please enter all required fields")
     }
     const service = await serviceModel.create({
-        name,
-        description,
-        price,
-        duration,
-        provider,
-        category,
-        user: req.user.id
+        name, description, price, duration, provider, category, user: req.user.id
     })
     if(service){
         res.status(201).json({
@@ -31,77 +26,74 @@ const createService = asyncHandler(async (req, res) => {
     }
 })
 
-//update new service
-const updateService = asyncHandler(async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ["name", "description", "price", "duration", "category"]
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-    if(!isValidOperation) {
-        return res.status(400).json({error: "Invalid updates!"})
-    }
-    try {
-        const service = await serviceModel.findOne({
-            _id: req.params.id,
-            freelancer: req.user._id
-        })
-        if(!service) {
-            return res.status(404).json({error : "The service you wanted to update does not exist"})
-        }
-        updates.forEach((update) => (service[update] = req.body[update]))
-        await service.save()
-        res.json(service)
-    } catch (err) {
-        res.status(400).json(err.message)        
-    }
+//update a service
+const updateService = asyncHandler( async( req, res ) => {
+    const service = await serviceModel.findById(req.params.id);
 
+    if(!service) {
+        res.status(404);
+        throw new Error("Service not found");  
+    };
 
-  //  try {
-    //     const updatedService = await serviceModel(req.params.id, req.body, { new: true })
-    //     res.status(200).json(updatedService)
-    // } catch (err) {
-    //     res.status(400)
-    //     throw new Error(err.message)
-    // }
-})
+    const user = await authModel.findById(req.user.id);
+
+    if(!user) {
+        res.status(404);
+        throw new Error("User not found");
+    };
+
+    // if(service.user !== user.id) {
+    //     res.status(401);
+    //     throw new Error(" User not authorized")
+    // };
+
+    const updatedService = await serviceModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(updatedService);
+});
 
 //delete service
-const deleteService = asyncHandler(async (req, res) => {
-    try {
-        await serviceModel.findByIdAndDelete({
-            _id: req.params.id,
-            freelancer: req.user._id
-        })
-        res.status(200).json("Service has been successfully deleted!")
-    } catch (err) {
-        res.status(400)
-        throw new Error(err.message) 
+const deleteService = asyncHandler( async( req, res ) => {
+    const service = await serviceModel.findById(req.params.id);
+    if(!service) {
+        res.status(404);
+        throw new Error("Service not found ");
     }
-})
+
+    const user = await authModel.findById(req.user.id);
+
+    if(!user) {
+        res.status(404);
+        throw new Error("User not found")
+    };
+
+    // if(service.user.toString() !== user.id) {
+    //     res.status(401);
+    //     throw new Error("User not authorized to perform this action");
+    // };
+
+    await service.remove();
+    res.status(200).json({ id: req.params.id })
+});
 
 //get single servive
 const getService = asyncHandler(async (req, res) => {
-    try {
-        const Service = await serviceModel.findOne({
-             _id: req.params.id,
-             freelancer: req.user._id
-        })
-        res.status(200).json(Service)
-    } catch (err) {
+    const service = await serviceModel.findById(req.params.id)
+    if(!service) {
         res.status(400)
-        throw new Error(err.message)
+        throw new Error("Service not found")
+    } else {
+        res.status(201).json(service)
     }
 })
 
 //get all services by one freelancer
 const getAllServices = asyncHandler(async (req, res) => {
-    try {
-        const Services = await serviceModel.find({ 
-            freelancer: req.user._id
-        })
-        res.status(200).json(Services)
-    } catch (err) {
+    const services = await serviceModel.find()
+    if (!services) {
         res.status(400)
-        throw new Error(err.message)
+        throw new Error("You have not added any services")
+    } else {
+        res.status(200).json(services)
     }
 })
 
