39f0be0fd8c398a8bdc4b8536a57680a3ba0f919
munene <munenenjue18@gmail.com>
2023-01-21 10:55:19 +0300

    made changes to how services are created and saved to DB

--- server/controllers/serviceController.js
+++ server/controllers/serviceController.js
@@ -3,15 +3,31 @@ const asyncHandler = require('express-async-handler')
 
 //create new service
 const createService = asyncHandler(async (req, res) => {
-    const service = new serviceModel({
-        ...req.body,
-        freelancer: req.user //this is done to associate the service with the freelancer who created it
+    const { name, description, price, duration, provider, category } = req.body
+    if(!name || !description || !price || !duration || !provider || !category) {
+        res.status(400)
+        throw new Error("Please enter all required fields")
+    }
+    const service = await serviceModel.create({
+        name,
+        description,
+        price,
+        duration,
+        provider,
+        category,
+        user: req.user.id
     })
-    try {
-        await service.save()
-        res.status(201).json(service)
-    } catch (err) {
-        res.status(400).json(err.message)        
+    if(service){
+        res.status(201).json({
+            _id: service.id,
+            name: service.name,
+            description: service.description,
+            duration: service.duration,
+            provider: service.provider,
+            category: service.category
+        })
+    } else {
+        res.status(400).json({error: "An error occurred"})
     }
 })
 
@@ -38,7 +54,7 @@ const updateService = asyncHandler(async (req, res) => {
         res.status(400).json(err.message)        
     }
 
-    
+
   //  try {
     //     const updatedService = await serviceModel(req.params.id, req.body, { new: true })
     //     res.status(200).json(updatedService)
