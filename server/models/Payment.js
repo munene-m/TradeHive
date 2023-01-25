95962e6803c2044589087fafdd35a644cc04b67f
munene <munenenjue18@gmail.com>
2023-01-25 22:58:30 +0300

    cleared some fields from payment model

--- server/models/Payment.js
+++ server/models/Payment.js
@@ -11,21 +11,11 @@ const PaymentSchema = mongoose.Schema({
         required: true,
         trim: true
     },
-    service: {
-        type: mongoose.Schema.Types.ObjectId, // Service id of the service that the user wants to pay off
-        ref: 'Service',
-        required: true
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Auth',
-        required: true
-    },
-    transactionCode: {
-        type: String, // String field to store the MPESA transaction code taht's generated after user makes payment.
-        required: true,
-        trim: true
-    },
+    // transactionCode: {
+    //     type: String, // String field to store the MPESA transaction code that's generated after user makes payment.
+    //     required: true,
+    //     trim: true
+    // },
     createdAt: {
         type: Date, //timestamp that stores the date and time the payment was created
         default: Date.now
