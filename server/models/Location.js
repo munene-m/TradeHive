46aa99131bf495532728deedb8042a5d391e30f5
munene <munenenjue18@gmail.com>
2023-01-28 22:42:15 +0300

    created the location model

--- server/models/Location.js
+++ server/models/Location.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose")
 
 const LocationSchema = mongoose.Schema({
-
+    town: {
+        type: String,
+        required: true
+    }
 })
 
 module.exports = mongoose.model("LocationSchema", LocationSchema)
