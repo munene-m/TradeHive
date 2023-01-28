a56fe9cec169bd6d98ae3c5f5a7777a3f20fba72
munene <munenenjue18@gmail.com>
2023-01-28 22:41:24 +0300

    password was causing the wrong credentials status 400 error. concatenated it correctly and the stk push works well

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -2,11 +2,6 @@ const paymentModel = require('../models/Payment')
 const asyncHandler = require('express-async-handler')
 const axios = require('axios')
 
-const shortcode = process.env.MPESA_PAYBILL
-const passkey = process.env.MPESA_PASSKEY
-
-
-
 const makePayment = asyncHandler( async (req, res) => {
 
     const phone = req.body.phone.substring(1)
@@ -19,49 +14,51 @@ const makePayment = asyncHandler( async (req, res) => {
         ("0" + date.getHours()).slice(-2) +
         ("0" + date.getMinutes()).slice(-2) +
         ("0" + date.getSeconds()).slice(-2)
+    const shortcode = process.env.MPESA_PAYBILL
+    const passkey = process.env.MPESA_PASSKEY
 
-    const password = new Buffer.from(shortcode + passkey + timestamp).toString("base64")
+    const password = new Buffer.from(`${shortcode}${passkey}${timestamp}`).toString("base64")
+    const url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+    const payload = {
+        "BusinessShortCode": 174379,
+        "Password": password,
+        "Timestamp": timestamp,
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": amount,
+        "PartyA": `254${phone}`,
+        "PartyB": 174379,
+        "PhoneNumber": `254${phone}`,
+        "CallBackURL": "https://1623-41-90-177-141.in.ngrok.io/payment/stk",
+        "AccountReference": "Test",
+        "TransactionDesc": "Test"
+    }
 
-    await axios.post(
-        "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
-        {
-            "BusinessShortCode": shortcode,
-            "Password": password,
-            "Timestamp": timestamp,
-            "TransactionType": "CustomerPayBillOnline",
-            "Amount": amount,
-            "PartyA": `254${phone}`,
-            "PartyB": shortcode,
-            "PhoneNumber": `254${phone}`,
-            "CallBackURL": "https://5532-41-90-187-218.in.ngrok.io/payment/stk",
-            "AccountReference": `254${phone}`,
-            "TransactionDesc": "Test"
-        },
+    await axios.post(url, payload,
         {
             headers: {
                 Authorization: `Bearer ${token}`
             }
         }
-    ).then((data) => {
-        console.log(data)
-        res.status(200).json(data)
+    ).then((response) => {
+        console.log(response.data)
+        res.status(200)
     })
     .catch((err) => {
         console.log(err)
         res.status(err.response.status).json(err.message)
     })
-    //     const payment = await paymentModel.create({
-    //         amount,
-    //         phone,
-    //     })
-    //     if (payment) {
-    //     return res.status(200).json({
-    //         _id: payment.id,
-    //         phone: payment.phone,
-    //         status: payment.status,
-    //         transactionId: payment.transactionId
-    //     })
-    // }
+        const payment = await paymentModel.create({
+            amount,
+            phone,
+        })
+        if (payment) {
+        return res.status(200).json({
+            _id: payment.id,
+            phone: payment.phone,
+            status: payment.status,
+            transactionId: payment.transactionId
+        })
+    }
 
 })
 
