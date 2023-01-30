e034178aa477f6a2e59767504b76d18a0e321f5b
munene-m <munenenjue@gmail.com>
2023-01-30 15:12:12 +0300

    added forgot password and update user functions to the auth controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -66,6 +66,53 @@ const loginUser = asyncHandler( async(req,res) => {
     }
 } )
 
+//forgot password
+const forgotPassword = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const userExists = authModel.findOne({email})
+        if(!userExists){
+            res.status(400).json({message: `The user ${email} does not exist`})
+        }
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const updatedUser = authModel.findOneAndUpdate(
+            { email },
+            { $set: { password: hashedPassword } },
+            { new: true }
+        )
+        res.status(200).json({
+            message: "Password reset was successful",
+            updatedUser,
+          });
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// update user details
+const updateUser = asyncHandler(async (req, res) => {
+    const { user_id } = req.params
+    const { firstname, lastname, email, profilePic } = req.body
+    try {
+      const userExists = await User.findById({ _id: user_id })
+      const updatedUser = await User.findOneAndUpdate(
+        {
+          _id: userExists._id,
+        },
+        {
+          $set: { firstname, lastname, email, profilePic },
+        },
+        { new: true }
+      );
+      return res.status(200).json({ message: "User details successfully updated", updatedUser })
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  })
+
 const getCredentials = asyncHandler (async (req, res) =>{
     res.status(200).json(req.user)
 })
@@ -79,5 +126,7 @@ const generateToken = (id) => {
 module.exports = {
     registerUser,
     loginUser,
+    forgotPassword,
+    updateUser,
     getCredentials
 }
