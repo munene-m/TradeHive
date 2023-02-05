a66791fae4fcb6e74a02e65bdac17d6bc40eeb3e
munene-m <munenenjue18@gmail.com>
2023-02-05 23:51:16 +0300

    added content to the home page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,8 +1,50 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
   <main>
-  <h1>Home page</h1>
+    <div class="container">
+      <div class="hero-content">
+        <h1>Get access to the brightest minds</h1>
+      <p>
+        Access Kenyan skilled individuals in various sectors at affordable rates.
+      </p>
+      <button>Get started</button>
+      </div>
+      <div class="community-img">
+        <img src="../assets/images/Group 5.png" alt="">
+      </div>
+    </div>
   </main>
 </template>
+
+<style scoped>
+main{
+  position: relative;
+  top:10rem;
+}
+.container{
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  align-items: center;
+  justify-items: center;
+  margin: 0 6em;
+}
+.hero-content{
+  padding: 0 3em;
+}
+.hero-content h1{
+  color: crimson;
+  font-size: 3.5rem;
+  text-align:start;
+}
+.hero-content p{
+  text-align: start;
+  font-weight: bold;
+  width:20rem;
+  /* margin: auto; */
+  font-size: 20px;
+}
+.community-img img{
+  padding:0 4em;
+}
+</style>
