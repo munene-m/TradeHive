3c25a3c828ed7b9af9870601c46343da43c201f1
munene-m <munenenjue18@gmail.com>
2023-02-07 13:04:45 +0300

    added urls to the auth urls file

--- client/src/utils/authUrls.js
+++ client/src/utils/authUrls.js
@@ -1,2 +1,5 @@
 export const registerUrl = "http://localhost:3000/auth/register";
 export const loginUrl = "http://localhost:3000/auth/login"
+export const forgotPasswordUrl = "http://localhost:3000/auth/forgot-password"
+export const userCredentialsUrl = "http://localhost:3000/auth/credentials"
+export const updateUserUrl = "http://localhost:3000/auth/update/:id"
