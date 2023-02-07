f1cfd265a970c5386ce0c5fb79687ee7faec4b84
munene-m <munenenjue18@gmail.com>
2023-02-07 19:41:05 +0300

    created a function that gets the first name of the logged in user and displays it in the welcome text on the home page

--- client/src/stores/auth.js
+++ client/src/stores/auth.js
@@ -1,36 +1,51 @@
 import { defineStore, acceptHMRUpdate } from "pinia";
 import { registerUrl, loginUrl } from "../utils/authUrls";
-import axios from 'axios'
+import axios from "axios";
 
 export const useAuthStore = defineStore({
   id: "auth",
   state: () => ({
-    user: JSON.parse(localStorage.getItem("token"))
+    user: JSON.parse(localStorage.getItem("token")),
+    userDetails: "",
   }),
 
-  getters:{
-
-  },
+  getters: {},
 
   actions: {
-    async register(firstname, lastname, email, password ){ 
-      const response = await axios.post(registerUrl, {firstname, lastname, email, password})
-      this.user = response.data.token
-      localStorage.setItem("token", JSON.stringify(response.data.token))
+    async register(firstname, lastname, email, password) {
+      const response = await axios.post(registerUrl, {
+        firstname,
+        lastname,
+        email,
+        password,
+      });
+      this.user = response.data.token;
+      localStorage.setItem("token", JSON.stringify(response.data.token));
     },
     async login(email, password) {
-      const response = await axios.post(loginUrl, {email, password})
-      this.user = response.data.token
-      localStorage.setItem("token", JSON.stringify(response.data.token))
-      console.log(this.user)
+      const response = await axios.post(loginUrl, { email, password });
+      this.user = response.data.token;
+      localStorage.setItem("token", JSON.stringify(response.data.token));
+      console.log(this.user);
     },
-    async logOut(){
-      this.user = null
-      localStorage.removeItem("token")
-    }
-  }
+    async logOut() {
+      this.user = null;
+      localStorage.removeItem("token");
+    },
+    async getUser() {
+      fetch("http://localhost:3000/auth/credentials", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${this.user}`,
+        },
+      })
+        .then((response) => response.json())
+        .then((response) => (this.userDetails = response.firstname))
+        .catch((err) => console.log(err));
+    },
+  },
 });
 
-if(import.meta.hot) {
-  import.meta.hot.accept(acceptHMRUpdate(useAuthStore, import.meta.hot))
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useAuthStore, import.meta.hot));
 }
--- client/src/views/HomePage.vue
+++ client/src/views/HomePage.vue
@@ -1,10 +1,15 @@
+<script setup> 
+import { useAuthStore } from "../stores/auth";
+const authStore = useAuthStore()
+</script>
+
 <template> 
-<h1>You are authenticated</h1>
+<h1>Welcome, {{ authStore.userDetails }}</h1>
 </template>
 
 <style>
 h1{
     position: relative;
-    top:10rem;
+    top:8rem;
 }
 </style>
