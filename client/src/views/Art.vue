2bccb77c8008fc53d15507d4d8d7bd04f9a49444
munene-m <munenenjue18@gmail.com>
2023-02-07 13:04:16 +0300

    created category routes

---
+++ client/src/views/Art.vue
@@ -0,0 +1,19 @@
+<template>
+    <h2>Recommended</h2>
+    <p>No services have been added yet</p>
+</template>
+
+<style scoped>
+h2{
+    position: relative;
+    top: 9rem;
+    margin: 0;
+    font-style: italic;
+    color: #626262;
+}
+p{
+    position: relative;
+    top:9rem;
+    color:#989898;
+}
+</style>
---
+++ client/src/views/HomePage.vue
@@ -0,0 +1,10 @@
+<template> 
+<h1>You are authenticated</h1>
+</template>
+
+<style>
+h1{
+    position: relative;
+    top:10rem;
+}
+</style>
