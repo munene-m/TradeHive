87da182ed050118ee93cdd521fae42ed0dacd538
munene-m <munenenjue18@gmail.com>
2023-02-07 13:01:36 +0300

    added route guards to some routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import { useAuthStore } from "../stores/auth";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: Home,
+      component: () => import("../views/Home.vue"),
     },
     {
       path: "/login",
@@ -17,9 +17,77 @@ const router = createRouter({
     {
       path: "/register",
       name: "register",
-      component: () => import("../views/Register.vue")
+      component: () => import("../views/Register.vue"),
+    },
+    {
+      path: "/wood-work",
+      name: "woodwork",
+      component: () => import("../views/WoodWork.vue"),
+      meta: {
+        auth: true
+      }
+    },
+    {
+      path: "/metal-work",
+      name: "metalwork",
+      component: () => import("../views/MetalWork.vue"),
+      meta: {
+        auth: true
+      }
+    },
+    {
+      path: "/art",
+      name: "art",
+      component: () => import("../views/Art.vue"),
+      meta: {
+        auth: true
+      }
+    },
+    {
+      path: "/painting",
+      name: "painting",
+      component: () => import("../views/Painting.vue"),
+      meta: {
+        auth: true
+      }
+    },
+    {
+      path: "/interior-design",
+      name: "interiordesign",
+      component: () => import("../views/InteriorDesign.vue"),
+      meta: {
+        auth: true
+      }
+    },
+    {
+      path: "/home-page",
+      name: "home-page",
+      component: () => import("../views/HomePage.vue"),
+      meta: {
+        auth: true
+      }
     }
   ],
 });
 
+const currentUser = () => {
+  return new Promise(( resolve, reject ) => {
+    const authStore = useAuthStore();
+    const token = authStore.user;
+    resolve(token);
+    reject
+  })
+}
+
+router.beforeEach( async (to, from, next) => {
+  if(to.matched.some((record) => record.meta.auth)) {
+    if(await currentUser()) {
+      next();
+    } else {
+      next("/")
+    }
+  } else {
+    next();
+  }
+})
 export default router;
