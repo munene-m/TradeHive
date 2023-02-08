d0d39dff37f8be6ba2ad701275530049c54ceab4
munene-m <munenenjue18@gmail.com>
2023-02-08 23:04:32 +0300

    added a welcome message that uses firstname of logged in user

--- client/src/views/HomePage.vue
+++ client/src/views/HomePage.vue
@@ -1,15 +1,40 @@
 <script setup> 
+import { onMounted } from "vue";
 import { useAuthStore } from "../stores/auth";
+import { useServiceStore } from "../stores/services"
 const authStore = useAuthStore()
+const serviceStore = useServiceStore()
+const getUser = authStore.getUser()
+
+onMounted(() => {
+    getUser
+})
 </script>
 
 <template> 
-<h1>Welcome, {{ authStore.userDetails }}</h1>
+<div class="header">
+    <h1>Welcome, {{ authStore.userDetails }}</h1>
+    <img src="../assets/images/Group 2.png" alt="">
+</div>
+<div v-if="authStore.roles = Client">
+
+</div>
 </template>
 
-<style>
-h1{
+<style scoped>
+.header{
     position: relative;
-    top:8rem;
+    top: 10rem;
+    margin-left: 6rem;
+    border-radius: 12px;
+    border: 1px solid #ccc;
+    width: 60%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px;
+}
+.header img{
+    width: 15%;
 }
 </style>
