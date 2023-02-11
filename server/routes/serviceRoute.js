78aaa0272a626eedd782a97fc4a7fb468054bad0
munene-m <munenenjue18@gmail.com>
2023-02-11 23:11:28 +0300

    removed protection from getservice functions

--- server/routes/serviceRoute.js
+++ server/routes/serviceRoute.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 const { protect } = require('../middleware/authMiddleware')
 const { createService, updateService, deleteService, getService, getAllServices } = require('../controllers/serviceController')
-router.route("/").get(protect, getAllServices)
+router.route("/").get( getAllServices)
 router.route("/create").post(protect, createService)
 router.route("/update/:id").put(protect, updateService)
 router.route("/delete/:id").delete(protect, deleteService)
-router.route("/service/:id").get(protect, getService)
+router.route("/service/:value").get(getService)
 // router.route("/myServices/:userId").get(protect, getAllServices)
 
 module.exports = router
