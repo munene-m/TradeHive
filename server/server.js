fca76c95580d5fd08555a695af5168fd5fe4ac67
munene-m <munenenjue18@gmail.com>
2023-02-12 13:59:11 +0300

    enabled express app instance to use body-parser

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require("cors")
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
 const port  = 3000
 const authRoute = require('./routes/authRoute')
 const serviceRoute = require('./routes/serviceRoute')
@@ -15,6 +16,7 @@ dotenv.config()
 app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({extended:false}))
+app.use(bodyParser.json())
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
