2aeeca281e2a5b60a5687b4b34e9ea484d815e6b
munene-m <munenenjue18@gmail.com>
2023-02-13 23:21:01 +0300

    created a settings view to complete profile creation

---
+++ client/src/views/AccountSettings.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { onMounted, ref, watchEffect, reactive } from "vue";
+import { useAuthStore } from "../stores/auth";
+const authStore = useAuthStore();
+const getUser = authStore.getUser();
+onMounted(() => {
+    getUser
+})
+</script>
+
+
+<template>
+    <div class="settings">
+        <h2>Complete setting up your account</h2>
+        <h1>{{ authStore.username.charAt(0).toUpperCase() }}</h1>
+        <h3>{{ authStore.username }}</h3>
+    </div>
+    <div v-if="authStore.clientRole">
+        <p>Client profile</p>
+    </div>
+    <div v-else-if="authStore.freelancerRole">
+        <p>Freelancer profile</p>
+    </div>
+</template>
+
+<style scoped>
+.settings{
+    position: relative;
+    top: 10rem;
+    margin: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+}
+.settings h1{ 
+    padding: 10px;
+    border: 1px solid #ccc;
+    height: 2em;
+    width: 2em;
+    border-radius: 50%;
+    background-color: #eee;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-weight: 600;
+    color: #888585;
+    margin:0;
+}
+settings h3{
+    margin:0;
+    text-transform: capitalize;
+}
+</style>
