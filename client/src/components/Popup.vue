1ad3011e3d8325a1db3e8a1be022e014e1e339fc
munene-m <munenenjue18@gmail.com>
2023-02-15 21:57:42 +0300

    created a popup component

---
+++ client/src/components/Popup.vue
@@ -0,0 +1,32 @@
+<template>
+    <div v-if="show" class="popup">
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const show = ref(true)
+defineProps({
+    message: String
+})
+onMounted(() => {
+    setTimeout(() => {
+        show.value = false
+    }, 3000)
+})
+</script>
+
+<style scoped>
+.popup{
+    position: fixed;
+    top: 10rem;
+    left: 50%;
+    transform: translateX(-50%);
+    padding: 10px;
+    background-color: #4CAF50;
+    color: #fff;
+    z-index: 10000;
+}
+</style>
