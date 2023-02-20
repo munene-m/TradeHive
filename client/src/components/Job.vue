db9a4edc5da033bcdde81c1a47a07fb7013058ca
munene-m <munenenjue18@gmail.com>
2023-02-20 22:09:35 +0300

    passed jobId as aprop from home-page and used it to get the service with that id to display its details

--- client/src/components/Job.vue
+++ client/src/components/Job.vue
@@ -1,10 +1,38 @@
 <script setup>
-defineProps({
-    jobId: String
-})
+import axios from "axios";
+import { ref, onMounted } from "vue";
+const jobs = ref([]);
 
+const props = defineProps({
+  jobId: {
+    type: String,
+    required: true,
+  },
+});
+onMounted(() => {
+  axios
+    .get(`http://localhost:3000/services/${props.jobId}`)
+    .then((response) => {
+      // Handle the response data
+      console.log(response.data);
+      jobs.value.push(response.data)
+    })
+    .catch((error) => {
+      // Handle the error
+      console.log(error);
+    });
+});
 </script>
 
 <template>
-
+  <div v-for="job in jobs" :key="jobId">
+    <p>Title: {{ job.name }}</p>
+  </div>
 </template>
+
+<style scoped>
+div {
+  position: relative;
+  top: 10rem;
+}
+</style>
