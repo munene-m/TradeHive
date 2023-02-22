a2901f599547f8ba949554dffa79671e1817c39e
munene-m <munenenjue18@gmail.com>
2023-02-22 17:37:57 +0300

    created a modal for the create Service form

---
+++ client/src/components/Modal.vue
@@ -0,0 +1,77 @@
+<script setup>
+defineProps({
+  show: Boolean,
+});
+</script>
+
+<template>
+  <div v-if="show" class="modal-mask">
+    <div class="modal-container">
+      <form id="createJobForm" @submit.prevent="handleSubmit">
+        <h3>Post a job</h3>
+        <p>Describe your needs then get contacted by freelancers</p>
+        
+        <label for="name">Your name</label><br />
+        <input type="text" id="name" name="name" />
+        
+        <label for="contact">Contact information</label><br />
+        <input type="text" id="contact" name="contact" />
+
+        <label for="category">Select relevant category so freelancers can find your job</label><br />
+        <select name="category" id="category">
+            <option value="default" selected>Choose</option>
+            <option value="Art">Art</option>
+            <option value="Wood work">Wood work</option>
+            <option value="Painting">Painting</option>
+            <option value="Interior design">Interior design</option>
+            <option value="Metal work">Metal work</option>
+        </select>
+        <p class="errorMsg"></p>
+        <br />
+
+        <label for="title">Job title</label><br />
+        <input type="text" id="title" name="title" />
+        <p class="errorMsg"></p>
+        <br />
+        
+        <label for="description">Job description</label><br />
+        <textarea name="description" id="description" cols="30" rows="10"></textarea>
+
+        
+        <label for="budget">Budget</label><br />
+        <input type="text" id="budget" name="budget" />
+
+        
+        <label for="currency">Currency</label><br />
+        <input type="text" id="currency" name="currency" />
+
+        <button id="createJobBtn" @click="$emit('close')" type="submit">
+          Log in
+        </button>
+      </form>
+      <!-- <footer>
+        <button >Submit</button>
+      </footer> -->
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.modal-mask {
+  position: fixed;
+  inset: 0;
+  background: rgb(0, 0, 0, 0.6);
+  backdrop-filter: blur(5px); /* Change blur amount as needed */
+  -webkit-backdrop-filter: blur(5px); /* For Safari */
+  display: grid !important;
+  place-items: center;
+  z-index: 1000;
+}
+.modal-container {
+  background: #fff;
+  padding: 1rem;
+  width: 80vw;
+  max-width: 800px;
+  border-radius: 5px;
+}
+</style>
