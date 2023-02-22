40aa86655808d32470aa5649339e48a1a56570f6
munene-m <munenenjue18@gmail.com>
2023-02-22 21:42:31 +0300

    progress styling the modal form

--- client/src/components/Modal.vue
+++ client/src/components/Modal.vue
@@ -10,21 +10,27 @@ defineProps({
       <form id="createJobForm" @submit.prevent="handleSubmit">
         <h3>Post a job</h3>
         <p>Describe your needs then get contacted by freelancers</p>
-        
-        <label for="name">Your name</label><br />
-        <input type="text" id="name" name="name" />
-        
-        <label for="contact">Contact information</label><br />
-        <input type="text" id="contact" name="contact" />
+        <div class="clientInfo">
+          <div>
+            <label for="name">Your name</label><br />
+            <input type="text" id="name" name="name" />
+          </div>
+          <div>
+            <label for="contact">Contact information</label><br />
+            <input type="text" id="contact" name="contact" />
+          </div>
+        </div>
 
-        <label for="category">Select relevant category so freelancers can find your job</label><br />
+        <label for="category"
+          >Select relevant category so freelancers can find your job</label
+        ><br />
         <select name="category" id="category">
-            <option value="default" selected>Choose</option>
-            <option value="Art">Art</option>
-            <option value="Wood work">Wood work</option>
-            <option value="Painting">Painting</option>
-            <option value="Interior design">Interior design</option>
-            <option value="Metal work">Metal work</option>
+          <option value="default" selected>Choose</option>
+          <option value="Art">Art</option>
+          <option value="Wood work">Wood work</option>
+          <option value="Painting">Painting</option>
+          <option value="Interior design">Interior design</option>
+          <option value="Metal work">Metal work</option>
         </select>
         <p class="errorMsg"></p>
         <br />
@@ -33,15 +39,18 @@ defineProps({
         <input type="text" id="title" name="title" />
         <p class="errorMsg"></p>
         <br />
-        
-        <label for="description">Job description</label><br />
-        <textarea name="description" id="description" cols="30" rows="10"></textarea>
 
-        
+        <label for="description">Job description</label><br />
+        <textarea
+          name="description"
+          id="description"
+          cols="30"
+          rows="10"
+        ></textarea>
+
         <label for="budget">Budget</label><br />
         <input type="text" id="budget" name="budget" />
 
-        
         <label for="currency">Currency</label><br />
         <input type="text" id="currency" name="currency" />
 
@@ -57,6 +66,17 @@ defineProps({
 </template>
 
 <style scoped>
+h3{
+    margin: 0;
+    font-weight: 800;
+    font-size: 2em;
+}
+.clientInfo {
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  gap: 20px;
+}
 .modal-mask {
   position: fixed;
   inset: 0;
