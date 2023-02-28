048714d8713260afece6dccf35d3991f60899ce5
munene-m <munenenjue18@gmail.com>
2023-02-28 10:41:36 +0300

    added some styling changes

--- client/src/views/AccountSettings.vue
+++ client/src/views/AccountSettings.vue
@@ -1,70 +1,85 @@
 <script setup>
 import { onMounted, ref, watchEffect, reactive, computed } from "vue";
 import { useAuthStore } from "../stores/auth";
-import { useVuelidate } from '@vuelidate/core'
-import { required, minLength, helpers } from '@vuelidate/validators'
+import { useVuelidate } from "@vuelidate/core";
+import { required, minLength, helpers } from "@vuelidate/validators";
 import Popup from "../components/Popup.vue";
 const authStore = useAuthStore();
 const getUser = authStore.getUser();
-const showPopUp = ref(false)
-
+const showPopUp = ref(false);
 
 const formData = reactive({
-    contact: "",
-    checkedCategories: []
-})
+  contact: "",
+  checkedCategories: [],
+});
 const rules = computed(() => {
-    return{
-        contact: { required: helpers.withMessage("Contact is required", required), minLength:minLength(10)}
-    }
-})
-const v$ = useVuelidate(rules, formData)
-const handleClientUpdate = async () =>{
-    const result = await v$.value.$validate()
-    if(result){
-        await authStore.updateClient(formData.checkedCategories, formData.contact)
-        
-    }
-    setTimeout(() => {
-        formData.contact = ""
-    }, 1000)
-}
+  return {
+    contact: {
+      required: helpers.withMessage("Contact is required", required),
+      minLength: minLength(10),
+    },
+  };
+});
+const v$ = useVuelidate(rules, formData);
+const handleClientUpdate = async () => {
+  const result = await v$.value.$validate();
+  if (result) {
+    await authStore.updateClient(formData.checkedCategories, formData.contact);
+  }
+  setTimeout(() => {
+    formData.contact = "";
+  }, 1000);
+};
 const freelanceFormData = reactive({
-    location: "",
-    contact: "",
-    workingHours: "",
-    rates: "",
-    checkedCategories: []
-})
+  location: "",
+  contact: "",
+  workingHours: "",
+  rates: "",
+  checkedCategories: [],
+});
 
 const freelanceRules = computed(() => {
-    return{
-        location: { required: helpers.withMessage("Location is required", required) },
-        workingHours: { required: helpers.withMessage("Working hours are required", required) },
-        rates: { required: helpers.withMessage("Rates are required", required), required },
-        contact: { required: helpers.withMessage("Contact is required", required), minLength:minLength(10)}
-    }
-})
-const v2$ = useVuelidate(freelanceRules, freelanceFormData)
+  return {
+    location: {
+      required: helpers.withMessage("Location is required", required),
+    },
+    workingHours: {
+      required: helpers.withMessage("Working hours are required", required),
+    },
+    rates: {
+      required: helpers.withMessage("Rates are required", required),
+      required,
+    },
+    contact: {
+      required: helpers.withMessage("Contact is required", required),
+      minLength: minLength(10),
+    },
+  };
+});
+const v2$ = useVuelidate(freelanceRules, freelanceFormData);
 const handleFreelancerUpdate = async () => {
-    const result = await v2$.value.$validate()
-    if(result){
-        authStore.updateFreelancer(freelanceFormData.location, freelanceFormData.workingHours, freelanceFormData.rates, freelanceFormData.checkedCategories, freelanceFormData.contact)
-        showPopUp.value = true
-    }
-    setTimeout(() => {
-        freelanceFormData.location =  "",
-        freelanceFormData.rates = "",
-        freelanceFormData.workingHours = "",
-        freelanceFormData.contact = "",
-        freelanceFormData.checkedCategories
-    }, 1000)
-}
-
+  const result = await v2$.value.$validate();
+  if (result) {
+    authStore.updateFreelancer(
+      freelanceFormData.location,
+      freelanceFormData.workingHours,
+      freelanceFormData.rates,
+      freelanceFormData.checkedCategories,
+      freelanceFormData.contact
+    );
+    showPopUp.value = true;
+  }
+  setTimeout(() => {
+    (freelanceFormData.location = ""),
+      (freelanceFormData.rates = ""),
+      (freelanceFormData.workingHours = ""),
+      (freelanceFormData.contact = ""),
+      freelanceFormData.checkedCategories;
+  }, 1000);
+};
 
 onMounted(() => {
   getUser;
-
 });
 </script>
 
@@ -251,34 +266,34 @@ onMounted(() => {
         </div>
       </div>
       <label for="location">Location</label><br />
-          <input
-            type="text"
-            v-model="freelanceFormData.location"
-            placeholder="e.g Ruiru, Kiambu"
-            id="location"
-            name="location"
-            autocomplete="off"
-          />
+      <input
+        type="text"
+        v-model="freelanceFormData.location"
+        placeholder="e.g Ruiru, Kiambu"
+        id="location"
+        name="location"
+        autocomplete="off"
+      />
       <!-- <p class="errorMsg" v-if="v$.location.$error">{{ v$.location.$errors[0].$message }}</p> -->
-          <label for="workingHrs">Working Hours</label><br />
-          <input
-            type="text"
-            v-model="freelanceFormData.workingHours"
-            placeholder="e.g 8am - 5pm"
-            id="workingHrs"
-            name="workingHrs"
-            autocomplete="off"
-          />
+      <label for="workingHrs">Working Hours</label><br />
+      <input
+        type="text"
+        v-model="freelanceFormData.workingHours"
+        placeholder="e.g 8am - 5pm"
+        id="workingHrs"
+        name="workingHrs"
+        autocomplete="off"
+      />
       <!-- <p class="errorMsg" v-if="v$.workingHours.$error">{{ v$.workingHours.$errors[0].$message }}</p> -->
-          <label for="rates">Rates</label><br />
-          <input
-            type="text"
-            v-model="freelanceFormData.rates"
-            placeholder="e.g Ksh 400/hr"
-            id="rates"
-            name="rates"
-            autocomplete="off"
-          />
+      <label for="rates">Rates</label><br />
+      <input
+        type="text"
+        v-model="freelanceFormData.rates"
+        placeholder="e.g Ksh 400/hr"
+        id="rates"
+        name="rates"
+        autocomplete="off"
+      />
       <!-- <p class="errorMsg" v-if="v$.rates.$error">{{ v$.rates.$errors[0].$message }}</p> -->
       <label for="phone">Contact information</label><br />
       <input
@@ -291,11 +306,10 @@ onMounted(() => {
       />
       <!-- <p class="errorMsg" v-if="v$.contact.$error">{{ v$.contact.$errors[0].$message }}</p> -->
 
-      <button type="submit"> Update</button>
+      <button type="submit">Update</button>
     </form>
-<Popup v-if="showPopUp" message="Account details updated successfully!"/>
+    <Popup v-if="showPopUp" message="Account details updated successfully!" />
   </div>
-  
 </template>
 
 <style scoped>
@@ -310,7 +324,7 @@ onMounted(() => {
 }
 .settings h1 {
   padding: 10px;
-  border: 2px solid #ccc;
+  border: 2px solid rgb(0, 166, 255);
   height: 2em;
   width: 2em;
   border-radius: 50%;
@@ -328,13 +342,12 @@ settings h3 {
 }
 #clientDetails {
   position: relative;
-  top: 8rem;
+  /* top: 8rem; */
   display: flex;
   justify-content: center;
   align-items: center;
 }
-#freelancerDetails{
-
+#freelancerDetails {
   display: flex;
   justify-content: center;
   align-items: center;
@@ -368,35 +381,39 @@ input[type="tel"] {
   border: 2px solid #bbb;
   line-height: 1.5em;
   background: #ededed;
-    line-height: 1.5rem;
-    border-radius: 4px;
-    padding: 6px;
-    margin-bottom: 18px;
+  line-height: 1.5rem;
+  border-radius: 4px;
+  padding: 6px;
+  margin-bottom: 18px;
+  transition: 0.4s;
 }
-
+input:focus {
+  background: #fff;
+  border: 2px solid rgb(0, 166, 255);
+}
 .checkboxes {
   display: grid;
   justify-items: start;
   grid-template-columns: repeat(3, 1fr);
   background: #eee;
-    border-radius: 6px;
-    padding: 10px;
-    margin-bottom:18px;
-    border: 2px solid #bbb;
-    gap:2px;
+  border-radius: 6px;
+  padding: 10px;
+  margin-bottom: 18px;
+  border: 2px solid #bbb;
+  gap: 2px;
 }
-button{
-    border: none;
-    outline: none;
-    color: white;
-    background: crimson;
-    padding: 8px 16px;
-    cursor: pointer;
-    border-radius: 6px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: auto;
-    margin-top: 1.5rem;
+button {
+  border: none;
+  outline: none;
+  color: white;
+  background: crimson;
+  padding: 8px 16px;
+  cursor: pointer;
+  border-radius: 6px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: auto;
+  margin-top: 1.5rem;
 }
 </style>
