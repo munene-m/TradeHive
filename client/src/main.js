f6b0e126725568146fd0bcd719a4111fcda7099d
munene-m <munenenjue18@gmail.com>
2023-03-18 22:59:36 +0300

    removed packages i had nstalled for animation and icons

--- client/src/main.js
+++ client/src/main.js
@@ -3,18 +3,10 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
-import { faStar as faStarRegular } from '@fortawesome/free-regular-svg-icons';
-
-library.add(faStar, faStarHalfAlt, faStarRegular);
 
 import '../src/assets/main.css'
 
 const app = createApp(App);
-
-app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.use(createPinia());
 app.use(router);
